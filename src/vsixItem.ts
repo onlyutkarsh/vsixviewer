0e086b82edf75afee48e27035ba5256e999e9898
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2019-10-19 19:28:09 +0100

    Feature/v1 (#1)
    * initial commit
    
    * webpack
    
    * using jszip
    
    * dir first sort + telemetry file ext for images
    
    * add gitversion
    
    * change pattern
    
    * add deploy task
    
    * remove gitversion

---
+++ src/vsixItem.ts
@@ -0,0 +1,54 @@
+import { TreeItem, TreeItemCollapsibleState, Uri, ThemeIcon } from "vscode";
+
+export class VsixItem extends TreeItem {
+    constructor(public readonly label: string, public collapsibleState: TreeItemCollapsibleState = TreeItemCollapsibleState.None) {
+        super(label, collapsibleState);
+    }
+    private _tooltip: string = "";
+    get tooltip(): string {
+        return this._tooltip;
+    }
+
+    set tooltip(v: string) {
+        this._tooltip = v;
+    }
+    private _size: number = 0;
+    public get size(): number {
+        return this._size;
+    }
+    public set size(v: number) {
+        this._size = v;
+    }
+    private _isDirectory: boolean = false;
+    public get isDirectory(): boolean {
+        return this._isDirectory;
+    }
+    public set isDirectory(v: boolean) {
+        this._isDirectory = v;
+    }
+    private _children: VsixItem[] = [];
+    public get children(): VsixItem[] {
+        return this._children;
+    }
+    public set children(v: VsixItem[]) {
+        this._children = v;
+    }
+    private _iconType: string = "";
+    public get iconType(): string {
+        return this._iconType;
+    }
+    public set iconType(v: string) {
+        this._iconType = v;
+    }
+
+
+    private _iconPath?: string | Uri | { light: string | Uri; dark: string | Uri; } | ThemeIcon | undefined = "";
+    public get iconPath(): string | Uri | { light: string | Uri; dark: string | Uri; } | ThemeIcon | undefined {
+        return this._iconPath;
+    }
+    public set iconPath(v: string | Uri | { light: string | Uri; dark: string | Uri; } | ThemeIcon | undefined) {
+        this._iconPath = v;
+    }
+
+    contextValue = "vsixItem";
+}
